1a3646778ea0c39c4a2ad2aff5562f8896bf80b7
FelicityBlue <73523510+FelicityBlue@users.noreply.github.com>
2025-03-02 12:57:13 -0500

    added functionity of timer(changing between 3 different modes, start timer), display total focus time, music player(start,stop)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Timer from './components/timer.jsx';
+import MusicPlayer  from './components/MusicPlayer.jsx';
 import './App.css'
 
 function App() {
+  
     return (
     <>
+    <h1 className="webName">Cute Study Timer</h1>
+    <MusicPlayer />
+    <Timer />
+    <a className="ghLink" href="https://github.com/FelicityBlue">@FelicityBlue</a>
     </>
   )
 }
---
+++ src/components/Timer.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react'
+import Tasks from "./Tasks";
+
+function Timer() {
+ const [timeLeft, setTimeLeft] = useState(1500);
+ const [secLeft, setSecLeft] = useState("00");
+ const [minLeft, setMinLeft] = useState(25);
+ const [timerRun, setTimerRun] = useState(false);
+
+ const [currTimer, setCurrTimer] = useState(25);
+ const [totalFocusTime, setTotalFocusTime] = useState(0);
+ 
+  function setSecStr(second){
+    if(second == 0){
+      setSecLeft("00");
+    }
+    else if(second < 10){
+      setSecLeft("0" + second);
+    }
+    else{
+      setSecLeft(second.toString());
+    }
+  }
+
+  function startTimer () {
+    setTimerRun(true);
+  }
+
+  useEffect(() => {
+    if(!timerRun) return;
+
+    const timeInterval = setInterval (() => {
+    if(timeLeft !== 0){
+        const updatedTimeLeft = timeLeft - 1;
+        setTimeLeft(updatedTimeLeft);
+
+        setSecStr(updatedTimeLeft % 60);
+        setMinLeft(Math.floor(updatedTimeLeft/60));
+    }
+    else{
+        setTimerRun(false);
+        setMinLeft(currTimer);
+        setTotalFocusTime(totalFocusTime+currTimer);
+        clearInterval(timeInterval);
+    }
+}, 1000);
+    return () => clearInterval(timeInterval);
+  }, [timerRun, timeLeft]);
+  function setTimer(seconds){
+    setTimeLeft(seconds);
+    setMinLeft(Math.floor(seconds / 60));
+    setSecLeft("00");
+    setCurrTimer(Math.floor(seconds / 60));
+    setTimerRun(false);
+  }
+    return (
+    <>
+    <div className="timerContainer">
+      <div>
+        <button className="timerBtn" onClick={() => setTimer(1500)}>pomodoro</button>
+        <button className="timerBtn" onClick={() => setTimer(300)}>short break</button>
+        <button className="timerBtn" onClick={() => setTimer(600)}>long break</button>
+      </div>
+      <p className="timer">{minLeft}:{secLeft}</p>
+    </div>
+    <div>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={startTimer}>Reset</button>
+    </div>
+
+    
+    <div id="totalTime">
+        <p>Total Focus Time: {totalFocusTime} minutes</p>
+    </div>
+
+    </>
+  )
+}
+
+export default Timer
