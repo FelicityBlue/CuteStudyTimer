f2fd86e3482dbe53f9c5a6f3ea062335cd6627e7
FelicityBlue <73523510+FelicityBlue@users.noreply.github.com>
2025-03-08 17:45:30 -0500

    Update: using Date.now to get more accurate time

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import settingIcon from "../assets/setting_icon.svg";
 function Timer() {
  const [timeLeft, setTimeLeft] = useState(1500);
@@ -6,12 +6,11 @@ function Timer() {
  const [totalFocusTime, setTotalFocusTime] = useState(0);
  
  let currTimer = 1500;
- let startTime = Date.now();
-
+ const startTimeRef  = useRef(0);
 
   function startTimer () {
     setTimerRun(true);
-    startTime = Date.now();
+    startTimeRef.current = Date.now();
   }
 
   useEffect(() => {
@@ -19,15 +18,18 @@ function Timer() {
 
     const timeInterval = setInterval (() => {
     if(timeLeft !== 0){
-        const updatedTimeLeft = timeLeft - 1;
+        const timePassed = Date.now() - startTimeRef.current;
+        const updatedTimeLeft = currTimer - (Math.floor(timePassed/1000));
+
         setTimeLeft(updatedTimeLeft);
     }
     else{
         setTimerRun(false);
+        setTimeLeft(currTimer);
         setTotalFocusTime(totalFocusTime+(Math.floor(currTimer / 60)));
         clearInterval(timeInterval);
     }
-}, 1000);
+}, 800);
     return () => clearInterval(timeInterval);
   }, [timerRun, timeLeft]);
   function setTimer(seconds){
@@ -36,21 +38,14 @@ function Timer() {
     setTimerRun(false);
   }
   function displayTime(){
-    let sec = "";
     let second = timeLeft % 60;
-    if(second == 0){
-      sec = "00";
-    }
-    else if(second < 10){
-      sec = "0" + timeLeft;
-    }
-    else{
-      sec = second.toString();
-    }
-
     let minute = Math.floor(timeLeft/60);
+    
+    // formatting the string
+    let sec = String(second).padStart(2, "0");
+    let min = String(minute).padStart(2,"0");
 
-    return `${minute}:${sec}`;
+    return `${min}:${sec}`;
   }
   function settingPopup(){
 
@@ -61,7 +56,7 @@ function Timer() {
         <div className="timerContainer">
           <div className="timerBtns">
             <button className="timerBtn" onClick={() => setTimer(1500)}>pomodoro</button>
-            <button className="timerBtn" onClick={() => setTimer(60)}>short break</button>
+            <button className="timerBtn" onClick={() => setTimer(300)}>short break</button>
             <button className="timerBtn" onClick={() => setTimer(600)}>long break</button>
           </div>
           <p className="timer">{displayTime()}</p>
