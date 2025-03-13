2644f632858d36743efe332f605a93a7618e1a27
FelicityBlue <73523510+FelicityBlue@users.noreply.github.com>
2025-03-13 16:24:01 -0400

    added style for spotify player

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     return (
     <>
     <h1 className="webName">Cute Study Timer</h1>
+    
     <div className="mainDisplayContainer">
       <div className="timerDisplayContainer" >
         <Timer />
@@ -18,7 +19,7 @@ function App() {
       </div>
     </div>
     <div className="musicPlayer">
-      <iframe id="spotify" width="100%" height="80" frameborder="0" allowfullscreen="" data-src="https://open.spotify.com/embed/playlist/4Zjli1P13J5mmSCD5iKAXK?theme=0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" src="https://open.spotify.com/embed/playlist/4Zjli1P13J5mmSCD5iKAXK?theme=0"></iframe>
+      <iframe id="spotify" data-src="https://open.spotify.com/embed/playlist/4Zjli1P13J5mmSCD5iKAXK?theme=0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" src="https://open.spotify.com/embed/playlist/4Zjli1P13J5mmSCD5iKAXK?theme=0"></iframe>
     </div>
     <a className="ghLink" href="https://github.com/FelicityBlue">@FelicityBlue</a>
     </>
