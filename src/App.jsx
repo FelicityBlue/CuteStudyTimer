87a22ddac9f4b3f3bc3b62e0c210be4d11b60e49
FelicityBlue <73523510+FelicityBlue@users.noreply.github.com>
2025-03-27 06:28:17 -0400

    putting music player inside the main display container

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,42 @@
-import { useState } from 'react';
-import Timer from './components/Timer.jsx';
-import Tasks from './components/Tasks.jsx';
+import { useState } from "react";
+import Timer from "./components/Timer.jsx";
+import Tasks from "./components/Tasks.jsx";
 import timeUpSound1 from "/src/assets/timeup_sound_1.mp3";
 import timeUpSound2 from "/src/assets/timeup_sound_2.mp3";
 
-import './App.css'
+import "./App.css";
 
 function App() {
   const currTimeUpSound = timeUpSound1;
-  
-    return (
+
+  return (
     <>
-    <h2 className="webName">Cute Study Timer</h2>
-    <div className="mainDisplayContainer">
-      <div className="timerDisplayContainer" >
-        <Timer timeUpSound={currTimeUpSound} />
+      <div className="container">
+        <h2 id="webName">Cute Study Timer</h2>
+        <div className="mainDisplayContainer">
+          <div className="timerDisplayContainer">
+            <Timer timeUpSound={currTimeUpSound} />
+          </div>
+          <div className="taskDisplayContainer">
+            <Tasks />
+          </div>
+        </div>
+
+        <div className="musicPlayer">
+          <iframe
+            id="spotify"
+            data-src="https://open.spotify.com/embed/playlist/4Zjli1P13J5mmSCD5iKAXK?theme=1"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            src="https://open.spotify.com/embed/playlist/4Zjli1P13J5mmSCD5iKAXK?theme=1"
+          ></iframe>
+        </div>
+
+        <a className="ghLink" href="https://github.com/FelicityBlue">
+          @FelicityBlue
+        </a>
       </div>
-      <div className="taskDisplayContainer">
-        <Tasks />
-      </div>
-    </div>
-    <div className="musicPlayer">
-      <iframe id="spotify" data-src="https://open.spotify.com/embed/playlist/4Zjli1P13J5mmSCD5iKAXK?theme=0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" src="https://open.spotify.com/embed/playlist/4Zjli1P13J5mmSCD5iKAXK?theme=0"></iframe>
-    </div>
-    <a className="ghLink" href="https://github.com/FelicityBlue">@FelicityBlue</a>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
